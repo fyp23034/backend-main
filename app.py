9af7c3f2adce875b3eec8a9ef095d39171484a2b
BrandonTay2001 <brandontay2001@gmail.com>
2024-02-19 19:46:57 +0800

    Add CORS support and preflight handling

--- app.py
+++ app.py
@@ -1,12 +1,24 @@
-from flask import Flask, Blueprint
+from flask import Flask, Blueprint, request, Response
+from flask_cors import CORS
 from emails import emails
 from metrics import metrics
 from pref import pref
 
 app = Flask(__name__)
+CORS(app, supports_credentials=True)
+app.config['CORS_HEADERS'] = 'Content-Type'
 app.register_blueprint(emails, url_prefix='/emails')
 app.register_blueprint(metrics, url_prefix='/metrics')
 app.register_blueprint(pref, url_prefix='/pref')
 
+@app.before_request
+def handle_preflight():
+    if request.method == "OPTIONS":
+        res = Response()
+        res.headers['X-Content-Type-Options'] = '*'
+        res.headers['Access-Control-Allow-Origin'] = '*'
+        res.headers['Access-Control-Allow-Headers'] = '*'
+        return res
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
